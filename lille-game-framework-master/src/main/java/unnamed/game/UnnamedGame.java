5ec135937da9c19e9239a874eefb45cbf4301bea
Haydgoki <noob-gamer@hotmail.fr>
2017-01-13 16:03:57 +0100

    Création d'une arborescence parallèle pour notre jeu

---
+++ lille-game-framework-master/src/main/java/unnamed/game/UnnamedGame.java
@@ -0,0 +1,18 @@
+package unnamed.game;
+
+import gameframework.game.GameData;
+import gameframework.game.GameDefaultImpl;
+
+public class UnnamedGame extends  GameDefaultImpl{
+	
+	
+	public UnnamedGame(GameData data) {
+		super(data);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("lol");
+	}
+}
+

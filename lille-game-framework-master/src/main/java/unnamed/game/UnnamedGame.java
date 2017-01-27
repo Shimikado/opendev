74d4f06dfc78a0b51c6c0bd3dc4625a22b8e032d
Shimikado <redant@a14p7.fil.univ-lille1.fr>
2017-01-27 13:47:05 +0100

    ajout doge qui bouge

---
+++ lille-game-framework-master/src/main/java/unnamed/game/UnnamedGame.java
@@ -0,0 +1,63 @@
+package unnamed.game;
+
+import java.net.MalformedURLException;
+
+import gameframework.game.*;
+import gameframework.gui.*;
+
+public class UnnamedGame extends  GameDefaultImpl{
+	
+	
+	public UnnamedGame(GameData data) {
+		super(data);
+		this.initLevels();
+	}	
+	/**
+	 * Add all the levels on the game list
+	 */
+	public void initLevels() {
+		this.data.addLevel(new TestLevel(data));
+	}
+
+	/**
+	 * @param args not use here 
+	 * @throws MalformedURLException
+	 */
+	public static void main(String[] args) throws MalformedURLException {
+		
+		GameConfiguration conf= new GameConfiguration(20,40,32,8);
+		
+		//T
+		GameData data = new GameData(conf);
+		
+		Game game = new UnnamedGame(data);
+		
+		GameStatusBarElement<Integer> score = new GameStatusBarElement<Integer>("Score : ", data.getScore());
+		
+		GameStatusBarElement<Integer> life = new GameStatusBarElement<Integer>("Life : ", data.getLife());
+			
+		GameWindow window = new GameWindow("Unnamed", data.getCanvas(),conf, life, score);
+			
+		window.createGUI();
+		game.start();
+	}
+}
+	
+	/*
+	SCHEMA D'UNE IMPLEMENTATION :
+	
+	-On crée le game + game configuration
+	
+	-On ajoute les levels et tout autre element dans le game data
+	
+	-gameConfiguration.createUniverse(gameData), gameConfiguration.createCanvas()
+	
+	
+	
+	On crée la GameWindow( Nom , Canvas, gameData)
+	On crée le GUI et on lance la fenêtre
+	
+	
+	Game.start(Nouvelle classe de game)  <-- lancer les observer les objets en mouvement etc etc et laisser faire les boucles de dessin(  ), et de mouvement (),
+	(LANCER BOUCLE)
+	*/
---
+++ lille-game-framework-master/src/main/java/unnamed/ressources/GameUniverseViewPortTest.java
@@ -0,0 +1,29 @@
+package unnamed.ressources;
+
+import gameframework.drawing.GameUniverseViewPortDefaultImpl;
+import gameframework.game.GameData;
+
+import java.net.URL;
+
+
+
+
+/**
+ * Cette classe à pour fonction de draw toutes les entitées et le background dans le canvas principal grace à sa méthode paint
+ * @author malamelli
+ *
+ */
+public class GameUniverseViewPortTest extends
+		GameUniverseViewPortDefaultImpl {
+
+	
+	
+	public GameUniverseViewPortTest(GameData data) {
+		super(data);
+	}
+
+	@Override
+	protected URL backgroundImage() {
+		return backgroundImage("/images/background.png");
+	}
+}
